f2198dd241386d85f6726f3ee2dae109ada6014d
Iamneo <techadmin@iamneo.ai>
2023-08-05 04:01:42 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/model/Task.java
+++ springapp/src/main/java/com/examly/springapp/model/Task.java
@@ -1,4 +1,11 @@
 package com.examly.springapp.model;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "task_details")
 
 public class Task {
     
